e720277ce9b1240aff4188668ebf40fc198324cb
lzhong <lzhong@andrew.cmu.edu>
2013-01-26 14:28:15 -0500

    lolwut

--- webapp/js/rowlogic.js
+++ webapp/js/rowlogic.js
@@ -4,6 +4,39 @@ var error = errorFn; //
 var print = printFn; // POST to web interface.
 var del = delRow;
 
+var makeRowsMobile = function(myDocs){
+    if (myDocs.length > 0){
+        $("#empty").hide();
+    }
+    for (var i=0; i<myDocs.length; i++){
+      var ftype = "unknown";
+      if (myDocs[i].mime_type === "application/msword" || myDocs[i].mime_type === "application/vnd.openxmlformats-officedocument.wordprocessingml.document"){
+          ftype = "msftword";
+      }
+      else if (myDocs[i].mime_type === "text/plain"){
+          ftype = "text";
+      }
+      else if (myDocs[i].mime_type.substring(0,6) === "image/"){
+          ftype = "image";
+      }
+      else if (myDocs[i].mime_type === "application/pdf" || myDocs[i].mime_type === "application/x-pdf"){
+          ftype = "pdf";
+      }
+      else if (myDocs[i].mime_type === "application/vnd.ms-powerpoint" || myDocs[i].mime_type === "pplication/vnd.openxmlformats-officedocument.presentationml.presentation"){
+          ftype = "msftppt";
+      }
+      document.write("<li id='row" + i.toString() +
+                     "'><a href='#'>" + 
+                     "<div class='tblrow'><img class='file-thumbnail' src='icons/" + ftype +
+                     ".jpg'><h5>" + myDocs[i].path + 
+                     "</h5><button class='print btn btn-success' type='button' onclick='print(\"" +
+                     i.toString() + "\")'>Print</button>" +
+                     "<button class='delete btn btn-danger' type='button' onclick='del(\"" +
+                     i.toString() + "\")'>Delete</button></div></a></li>");
+    };
+    return;
+}
+
 function makeRowFn(myDocs){
     if (myDocs.length > 0){
         $("#empty").hide();
@@ -72,8 +105,6 @@ function delRow(rowId){
         delButton.fadeIn("slow");
     }
     //printButton.fadeIn("slow");
-
-
 
     if ($(".tblrow").length <= 0) {
         $("#empty").show();
