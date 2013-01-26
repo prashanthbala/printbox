b03650f74407bcb4ca7824c994aa468eee5a9138
lzhong <lzhong@andrew.cmu.edu>
2013-01-26 03:43:27 -0500

    um...

--- webapp/js/rowlogic.js
+++ webapp/js/rowlogic.js
@@ -1,18 +1,18 @@
 var makeRows = function(myDocs){
     for (var i=0; i<myDocs.length; i++){
-      var ftype = "unknown";
-      if (myDocs[i].filetype === "image"){
+      var ftype = "sample";
+      if (myDocs[i].mime_type === "image"){
           ftype = "image";
       }
-      else if (myDocs[i].filetype === "text"){
+      else if (myDocs[i].mime_type === "text"){
           ftype = "text";
       }
-      else (myDocs[i].filetype === "multimedia"){
+      else (myDocs[i].mime_type === "multimedia"){
           ftype = "multimedia";
       }
       document.write("<li><a href='#'>" + 
                      "<div class='tblrow'><img src='icons/" + ftype + ".jpg'><h5>" + 
-                     myDocs[i].filename + 
+                     myDocs[i].path + 
                      "</h5><button class='btn btn-success' type='button'>Print</button>" +
                      "<button class='btn btn-danger' type='button'>Delete</button></div></a></li>");
     }
