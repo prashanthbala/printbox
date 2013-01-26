25561685674aefc4bcab8cb62c50182cf76e7209
lzhong <lzhong@andrew.cmu.edu>
2013-01-26 02:58:35 -0500

    added more row logic js

--- webapp/js/rowlogic.js
+++ webapp/js/rowlogic.js
@@ -1,6 +1,23 @@
-var makeRows = function(){
-    while (condition){
-      code block to be executed
+var makeRows = function(myDocs){
+    for (var i=0; i<myDocs.length; i++){
+      var ftype = "unknown";
+      if (myDocs[i].filetype === "image"){
+          ftype = "image";
+      }
+      else if (myDocs[i].filetype === "text"){
+          ftype = "text";
+      }
+      else (myDocs[i].filetype === "multimedia"){
+          ftype = "multimedia";
+      }
+      document.write("<li><a href='#'>" + 
+                     "<div class='tblrow'><img src='icons/" + ftype + ".jpg'><h5>" + 
+                     myDocs[i].filename + 
+                     "</h5><button class='btn btn-success' type='button'>Print</button>" +
+                     "<button class='btn btn-danger' type='button'>Delete</button></div></a></li>");
     }
-    return
+    return;
 }   
+var error = function(){
+  document.write("<li>Super Sad Faces Everywhere</li>");
+}
